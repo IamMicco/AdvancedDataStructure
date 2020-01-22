94d6f4c6a5f9b2ad6e9108d9aa1e0da219b1f4ac
Michael Nwabuobi <michael@localhost.localdomain>
2020-01-22 13:18:27 -0600

    made some changes

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment;
 
 /**
@@ -11,11 +6,25 @@ package assignment;
  */
 public class Assignment {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	private int[] PIN;
+	private int[] password;
+
+	Assignment () {
+
+		this.PIN = new int[5];
+	}
+
+	public void setPIN (int[] pin) {
+
+		this.PIN = pin;
+	}
+
+	private int[] createPassword () {
+
+	}
+
+    	public static void main(String[] args) {
+
+    	}
+
 }
