e8f314e9abd8bad7095662523225ad5edf1dc6ff
root <root@localhost.localdomain>
2020-01-23 09:46:49 -0600

    creating authentication utility class

---
+++ Assignment/src/assignment1/Assignment.java
@@ -0,0 +1,30 @@
+package assignment1;
+
+/**
+ *
+ * @author Michael Nwabuobi
+ */
+public class Assignment {
+
+	private int[] PIN;
+	private int[] password;
+
+	Assignment () {
+
+		this.PIN = new int[5];
+	}
+
+	public void setPIN (int[] pin) {
+
+		this.PIN = pin;
+	}
+
+	private int[] createPassword () {
+
+	}
+
+    	public static void main(String[] args) {
+
+    	}
+
+}
---
+++ Assignment/src/assignment1/Authentication.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public static class Authentication {
+
+	public static generateSequence () {
+
+
+	}
+
+	public static verify (int[] correctPIN, int[] userResponsePIN) {
+
+
+	}
+}
