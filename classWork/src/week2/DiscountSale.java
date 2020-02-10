9ecb3d8d8f1945944cdd7787bfe77092077111c8
Michael Nwabuobi <michael.nwabuobi@gmail.com>
2020-02-09 18:45:46 -0600

    add some classWork material

---
+++ classWork/src/week2/DiscountSale.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2;
+
+/**
+ *
+ * @author Michael Nwabuobi
+ */
+public class DiscountSale extends Sale {
+    
+    private double discount;
+    
+    public DiscountSale (String name, double price, double discount) {
+        
+        super(name, price);
+        setDiscount(discount);
+    }
+    
+    public DiscountSale (DiscountSale otherDSale) {
+        
+        super(otherDSale);
+        this.discount = otherDSale.discount;
+    }
+    
+    public void setDiscount (double discount) {
+        
+        this.discount = discount;
+    }
+    
+    public double bill () {
+        
+        double fraction = discount/100;
+        return (1 - fraction) * getPrice();
+    }
+    
+    public static void announcement () {
+        
+        System.out.println("This is discount sale");
+    }
+    
+    public String toString () {
+        
+        return (getName() + " Price = $" + getPrice()
+                + "Discoount = " + discount + "%\n");
+    }
+}
---
+++ classWork/src/week4/PriorityQueue.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author Michael Nwabuobi
+ */
+public class PriorityQueue<T extends Comparable<T>> {
+    
+    private T[] pqArray;
+    private int capacity, qSize, index;
+    private Class<T> cl;
+    
+    public PriorityQueue (Class<T> cl, int capacity) {
+        
+        this.cl = cl;
+        this.capacity = capacity;
+        pqArray = (T[])Array.newInstance(cl, capacity);
+        qSize = 0;
+        index = 0;
+    }
+    
+    public boolean isEmpty () {
+        
+        return qSize == 0;
+    }
+    
+    public int getSize () {
+        
+        return qSize; 
+    }
+    
+    public T peek () {
+        
+        if (qSize == 0) {
+            
+            System.out.println("Queue is Empty");
+            return null;
+        }
+        int maxIndex = 0;
+        for (int i = 0; i < qSize; i++) {
+            
+            if (pqArray[i].compareTo(pqArray[maxIndex]) > 0) {
+                
+                maxIndex = i;
+            }
+        }
+        T result = pqArray[maxIndex];
+        return result;
+    }
+    
+    public void clear () {
+        
+        pqArray = (T[])Array.newInstance(cl, capacity);
+        qSize = 0;
+    }
+    
+    public boolean isFull () {
+        
+        return qSize == capacity;
+    }
+    
+    public void add (T data) {
+        
+        if (qSize == capacity) System.out.println("Queuse is full");
+        pqArray[index++] = data;
+        qSize++;
+        System.out.println(String.format("Added element at index %d", index));
+    }
+    
+    public T remove () {
+        
+        if (qSize == 0) {
+            
+            System.out.println("Queue is Empty");
+            return null;
+        }
+        int maxIndex = 0;
+        for (int i = 0; i < qSize; i++) {
+            
+            if (pqArray[i].compareTo(pqArray[maxIndex]) > 0) {
+                
+                maxIndex = i;
+            }
+        }
+        T result = pqArray[maxIndex];
+        index--;
+        pqArray[maxIndex] = pqArray[index];
+        qSize--;
+        return result;
+    }
+}
