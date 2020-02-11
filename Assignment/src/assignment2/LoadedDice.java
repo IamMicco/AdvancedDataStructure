09736fe408c8b3954bd311342a41aec7b94c7a96
Michael Nwabuobi <michael.nwabuobi@gmail.com>
2020-02-11 12:14:22 -0600

    completed question 2 of assignment

---
+++ Assignment/src/assignment2/LoadedDice.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Michael Nwabuobi
+ */
+public class LoadedDice extends Random {
+    
+    public LoadedDice () {
+        
+        super();
+    }
+    
+    @Override
+    public int nextInt(int num) {
+        
+        int nextInt = super.nextInt(num);
+        int random = (int) (Math.random() * 2);
+        System.out.println(String.format("This is the random value: %d", random));
+        if (random == 1) return ++num;
+        else return nextInt;
+    }
+    
+    public static void main (String[] args) {
+        
+        LoadedDice dice = new LoadedDice();
+        System.out.println(dice.nextInt(100));
+    }
+}
---
+++ classWork/src/week5/PoliceThief.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+/**
+ *
+ * @author Michael Nwabuobi
+ */
+public class PoliceThief {
+    
+    public static void catchThief (char[] arr, int n, int k) {
+        
+        for (int i = 0; i < arr.length; i++) {
+            
+            int count = 0;
+            if (arr[i] == 'P') {
+                
+                for (int j = 0; j < k; j++) {
+                    
+                    
+                }
+            }
+        }
+    }
+}
