d35d57a7ac725836fc27e4471c722fca54aac2cc
Michael Nwabuobi <michael.nwabuobi@gmail.com>
2020-02-24 21:36:18 -0600

    added something

--- Assignment/src/assignment3/question2/SmartBinaryHeap.java
+++ Assignment/src/assignment3/question2/SmartBinaryHeap.java
@@ -1,6 +1,9 @@
 package assignment3.question2;
 
-public class SmartBinaryHeap extends BinaryHeap { 
+public class SmartBinaryHeap <T extends Comparable<T>> extends BinaryHeap <T> {
 
-
+    public SmartBinaryHeap(Class<T> cl, int capacity) {
+        super(cl, capacity);
+        // TODO Auto-generated constructor stub
+    }
 }
