189af60621de1893a042e019dd6eac06ec51e594
Michael Nwabuobi <michael.nwabuobi@gmail.com>
2020-04-22 21:10:22 -0500

    Testing logger library

--- Project/DistributedSystems/src/distributedsystems/Testing.java
+++ Project/DistributedSystems/src/distributedsystems/Testing.java
@@ -23,7 +23,7 @@ public class Testing {
             FileHandler fh = new FileHandler("myLogger.log", true);
             fh.setFormatter(new SimpleFormatter() {
 
-                private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+                private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %4$d %n";
 
                 @Override
                 public synchronized String format(LogRecord lr) {
@@ -31,7 +31,8 @@ public class Testing {
                     return String.format(format, 
                             new Date(lr.getMillis()),
                             lr.getLevel().getLocalizedName(),
-                            lr.getMessage());
+                            lr.getMessage(),
+                            1289);
                 }
             });
             fh.setLevel(Level.FINE);
